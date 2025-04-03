aa04364c66fb7022847e0eed09af2b207b2057cc
Erick Gualpa <m.erickgualpa@gmail.com>
2025-04-04 00:05:14 +0200

    Move BusinessResponse into GetBusinessController

--- business-service/src/main/kotlin/org/egualpam/contexts/location/business/adapters/in/controllers/GetBusinessController.kt
+++ business-service/src/main/kotlin/org/egualpam/contexts/location/business/adapters/in/controllers/GetBusinessController.kt
@@ -1,5 +1,6 @@
 package org.egualpam.contexts.location.business.adapters.`in`.controllers
 
+import org.egualpam.contexts.location.business.application.ports.`in`.query.BusinessDto
 import org.egualpam.contexts.location.business.application.ports.`in`.query.RetrieveBusiness
 import org.egualpam.contexts.location.business.application.ports.`in`.query.RetrieveBusinessQuery
 import org.slf4j.Logger
@@ -32,5 +33,27 @@ class GetBusinessController(
       logger.error("Error retrieving business with id [$id]", e)
       internalServerError().build()
     }
+  }
+}
+
+data class BusinessResponse(
+  val id: String,
+  val address: String,
+  val city: String,
+  val state: String,
+  val country: String,
+  val latitude: String,
+  val longitude: String
+) {
+  companion object {
+    fun from(dto: BusinessDto) = BusinessResponse(
+        id = dto.id,
+        address = dto.address,
+        city = dto.city,
+        state = dto.state,
+        country = dto.country,
+        latitude = dto.latitude,
+        longitude = dto.longitude,
+    )
   }
 }
