8ddfbe56f03c51c54356829a255c7573436ca514
Erick Gualpa <m.erickgualpa@gmail.com>
2025-04-07 21:43:53 +0200

    Use ExecutorService from BusinessesInitializer

--- business-service/src/main/kotlin/org/egualpam/contexts/location/business/adapters/in/initializers/BusinessesInitializer.kt
+++ business-service/src/main/kotlin/org/egualpam/contexts/location/business/adapters/in/initializers/BusinessesInitializer.kt
@@ -5,6 +5,7 @@ import org.egualpam.contexts.location.business.application.ports.`in`.command.Cr
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.transaction.support.TransactionTemplate
 import java.util.UUID.randomUUID
+import java.util.concurrent.Executors
 import kotlin.random.Random
 
 class BusinessesInitializer(
@@ -12,23 +13,28 @@ class BusinessesInitializer(
   private val createBusiness: CreateBusiness
 ) : InitializingBean {
   override fun afterPropertiesSet() {
-    repeat(100_000) {
-      val latitude = Random.nextDouble(-90.0, 90.0)
-      val longitude = Random.nextDouble(-180.0, 180.0)
+    Executors.newFixedThreadPool(10)
+        .use { executorService ->
+          repeat(100_000) {
+            val latitude = Random.nextDouble(-90.0, 90.0)
+            val longitude = Random.nextDouble(-180.0, 180.0)
 
-      val command = CreateBusinessCommand(
-          id = randomUUID().toString(),
-          addressStreet = "Address $it",
-          addressState = "State $it",
-          addressCity = "City $it",
-          addressCountry = "Country $it",
-          locationLatitude = latitude,
-          locationLongitude = longitude,
-      )
+            val command = CreateBusinessCommand(
+                id = randomUUID().toString(),
+                addressStreet = "Address $it",
+                addressState = "State $it",
+                addressCity = "City $it",
+                addressCountry = "Country $it",
+                locationLatitude = latitude,
+                locationLongitude = longitude,
+            )
 
-      transactionTemplate.executeWithoutResult {
-        createBusiness.execute(command)
-      }
-    }
+            executorService.submit {
+              transactionTemplate.execute {
+                createBusiness.execute(command)
+              }
+            }
+          }
+        }
   }
 }
