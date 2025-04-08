d09781bb07dc8bcefb7f3186913ce6389d01e429
Erick Gualpa <m.erickgualpa@gmail.com>
2025-04-08 23:17:26 +0200

    Setup business-location-service Spring Boot application

---
+++ business-location-service/src/main/kotlin/org/egualpam/contexts/location/shared/BusinessLocationServiceSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package org.egualpam.contexts.location.shared
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusinessLocationServiceSpringBootApplication
+
+fun main(args: Array<String>) {
+  runApplication<BusinessLocationServiceSpringBootApplication>(*args)
+}
